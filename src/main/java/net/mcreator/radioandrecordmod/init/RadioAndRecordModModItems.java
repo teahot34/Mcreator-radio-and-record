ada1e1b594612aa36befc48c2970c2549cec909f
teahot34 <tom.beaujean@free.fr>
2025-09-26 17:38:59 +0200

    Test avec ajout du bloc

---
+++ src/main/java/net/mcreator/radioandrecordmod/init/RadioAndRecordModModItems.java
@@ -0,0 +1,29 @@
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.radioandrecordmod.init;
+
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.BlockItem;
+
+import net.mcreator.radioandrecordmod.RadioAndRecordModMod;
+
+public class RadioAndRecordModModItems {
+	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, RadioAndRecordModMod.MODID);
+	public static final RegistryObject<Item> D = block(RadioAndRecordModModBlocks.D);
+
+	// Start of user code block custom items
+	// End of user code block custom items
+	private static RegistryObject<Item> block(RegistryObject<Block> block) {
+		return block(block, new Item.Properties());
+	}
+
+	private static RegistryObject<Item> block(RegistryObject<Block> block, Item.Properties properties) {
+		return REGISTRY.register(block.getId().getPath(), () -> new BlockItem(block.get(), properties));
+	}
+}
